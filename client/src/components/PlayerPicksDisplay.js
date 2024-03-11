4056295d37c0b3bed3cc40505c51e5104edf614d
Elan Zinkow <ezinkow@gmail.com>
2024-03-11 13:07:11 -0500

    m

--- client/src/components/PlayerPicksDisplay.js
+++ client/src/components/PlayerPicksDisplay.js
@@ -5,14 +5,15 @@ import Table from 'react-bootstrap/Table';
 
 export default function PicksDisplay() {
     const [picks, setPicks] = useState([])
+    const [games, setGames] = useState([])
 
     useEffect(() => {
         async function fetchPicks() {
             try {
                 const response = await axios(`api/picksdisplay`)
                 console.log('response', response)
-                const sortedList = response.data.sort((a, b) => (a.game_id > b.game_id) ? 1 : (a.game_id === b.game_id) ? ((a.name > b.name) ? 1 : -1) : -1)
-                setPicks(sortedList)
+                setPicks(response.data)
+                console.log('picks', picks)
             } catch (e) {
                 console.log(e)
             }
@@ -20,6 +21,18 @@ export default function PicksDisplay() {
         fetchPicks()
     }, [])
 
+    useEffect(() => {
+        async function fetchGames() {
+            try {
+                const response = await axios(`api/games`)
+                console.log('response', response)
+                setGames(response.data)
+            } catch (e) {
+                console.log(e)
+            }
+        }
+        fetchGames()
+    }, [])
     return (
 
         <div className="table">
@@ -29,16 +42,20 @@ export default function PicksDisplay() {
                     <thead>
                         <tr>
                             <th key='game id'>Name</th>
-                            <th key='game id'>Game #</th>
-                            <th key='game pick'>Pick</th>
+                            {games.length > 0 ? games.map(game =>
+                                <th key='game pick'>{game.underdog} vs {game.favorite}</th>
+                            ) : ""
+                            }
                         </tr>
                     </thead>
                     <tbody>
                         {picks.length > 0 ? picks.map(thisPick =>
                             <tr>
                                 <td key={thisPick.name}>{thisPick.name}</td>
-                                <td key={thisPick.game_id}>{thisPick.game_id}</td>
-                                <td key={thisPick.pick}>{thisPick.pick}</td>
+                                {thisPick.length > 0 ? thisPick.map(singlePick =>
+                                    <td key={thisPick.id}>{singlePick.pick}</td>
+                                ) : ""
+                                }
                             </tr>
                         ) : ""
                         }
