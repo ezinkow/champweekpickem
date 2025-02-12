ad206cb09e054650fb4991f18615cdde3d6cecf5
Elan Zinkow <ezinkow@gmail.com>
2025-02-12 17:24:49 -0600

    form edits

--- client/src/components/Instructions.js
+++ client/src/components/Instructions.js
@@ -6,7 +6,7 @@ export default function instructions() {
             <h3>Steps:</h3>
             <ul>
                 <li>Select CORRECT name from drop down</li>
-                <li>Make your picks. Try to do them in order. You can always go back and change your pick</li>
+                <li>Make your picks. Try to do them in order. You can edit picks</li>
                 <li>Picks will show up in the table down below</li>
                 <li>When ready, submit your picks. If you need to make a change, just reach out to me. Do not re-submit picks unless I tell you to</li>
                 <li>The google doc will still house your picks and results</li>
