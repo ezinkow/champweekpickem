0600441351f8af17d690c315cef9042727b6ac9e
Elan Zinkow <ezinkow@gmail.com>
2025-02-27 10:56:00 -0600

    game rules

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,7 +11,21 @@ export default function Home() {
         <div>
             <Navbar />
             <div className='container'>
-               
+                The game:
+                Pick every conference tournamet game <u>against the spread</u> from the following conferences:
+                American
+                ACC
+                Big East
+                Big 10
+                Big 12
+                SEC
+                A10 (new this year)
+                Mountain West (new this year)
+                <br></br>
+                <br></br>
+                AND ALL conference championship games between March 8th and 16th.
+                <br></br>
+                <br></br>
                 <Link to="/picksam"><h3>Make Today's AM Picks</h3></Link>
                 <Link to="/pickspm"><h3>Make Today's PM Picks</h3></Link>
                 <br></br>
