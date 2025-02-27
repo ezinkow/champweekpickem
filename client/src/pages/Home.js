1109de458cc27a76ec0e8af1d9c3f9ddf8ae7368
Elan Zinkow <ezinkow@gmail.com>
2025-02-27 10:32:09 -0600

    schedule link

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,23 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import { Link } from 'react-router-dom'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-
-// const [links, setLinks] = useState('ncaa.com')
-
-// useEffect(() => {
-//     async function fetchLinks() {
-//         try {
-//             const response = await axios("api/links/")
-//             setLinks(response.data.url)
-//             console.log(links)
-//         } catch (e) {
-//             console.log(e)
-//         }
-//     }
-//     fetchLinks()
-// }, [])
+import NameSubmit from '../components/NameSubmit'
 
 
 export default function Home() {
@@ -27,14 +11,16 @@ export default function Home() {
         <div>
             <Navbar />
             <div className='container'>
+                <h3>Check out the google doc for your picks results:</h3>
+                <a href='https://docs.google.com/spreadsheets/d/1Lrf8mPnABvaX4TH1VlAf88yOWchWGz49sEbH_WUyG2w/edit#gid=0' target="_blank">https://docs.google.com/spreadsheets/d/1Lrf8mPnABvaX4TH1VlAf88yOWchWGz49sEbH_WUyG2w/edit#gid=0</a>
                 <br></br>
                 <br></br>
-                <Link to="/picksam"><h3>Make Today's AM Picks</h3></Link>
-                <Link to="/pickspm"><h3>Make Today's PM Picks</h3></Link>
+                <Link to="/picksam"><h3>Make Today's Pick</h3></Link>
+                {/* <Link to="/pickspm"><h3>Make Today's PM Picks</h3></Link> */}
                 <br></br>
                 <br></br>
                 <h5>Conference Tournament Schedules:</h5>
-                <a href='https://www.ncaa.com/news/basketball-men/article/2025-03-16/2025-ncaa-conference-tournaments-schedules-brackets-scores-auto-bids' target="_blank">FULL LIST OF CONFERENCE TOURNAMENTS</a>
+                <a href='https://www.ncaa.com/news/basketball-men/article/2025-02-12/2025-ncaa-conference-tournaments-schedules-brackets-scores-auto-bids' target="_blank">FULL LIST OF CONFERENCE TOURNAMENTS</a>
             </div>
         </div>
     )
