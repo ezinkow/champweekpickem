f7730f4fbd12ed477fb7b32c209adfc4bab46600
Elan Zinkow <ezinkow@gmail.com>
2025-02-28 13:39:00 -0600

    m

--- client/src/components/Instructions.js
+++ client/src/components/Instructions.js
@@ -6,10 +6,11 @@ export default function instructions() {
             <h3>Steps:</h3>
             <ul>
                 <li>Select CORRECT name from drop down</li>
-                <li>Make your picks. Try to do them in order. You can edit picks</li>
+                <li>Make your picks</li>
                 <li>Picks will show up in the table down below</li>
-                <li>When ready, submit your picks. If you need to make a change, just reach out to me. Do not re-submit picks unless I tell you to</li>
-                <li>The google doc will still house your picks and results</li>
+                <li>When ready, submit your picks. If you need to make a change, you can resubmit single picks/games. Most recent pick will be official pick</li>
+                <li>Lines that are whole numbers will shift .5 in favor of the underdog (-5 moves to -5.5)</li>
+                <li>Missed picks will default to underdog with a single point penalty (+3.5 will move to +2.5)</li>
             </ul>
         </div>
 
